54c86b51ab86da86b2692f5dcb9fd7a161548da6
Jaki Widby <jaki.widby@gmail.com>
2020-08-31 11:26:11 -0700

    set up environment

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+import { Triangle } from './triangle.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
 $(document).ready(function() {
   $("#triangle").submit(function(event) {
     event.preventDefault();
